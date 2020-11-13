a03190008ae4cc5b4144369a117963f7ae20bf97
Douglas Oliveira <douglas.aguiar410@gmail.com>
2020-11-13 16:53:26 -0300

    ✅ test: Updated unit test

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,19 +1,20 @@
 import { StormGlass } from '@src/clients/stormGlass';
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
-import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import axios from 'axios';
 
 jest.mock('axios');
 
-describe('stormGlass client', () => {
-  it('Should return the normalized forecast from the StormGlass service', async() => {
+describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
 });
